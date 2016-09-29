178d88c3c4dbe3c285c6d964bb8a4985a1366de0
Mathias Ducancel <mathiasduc@gmail.com>
2016-09-29 16:41:25 +0200

    Add more jQuery exercises

--- jquery101/app.js
+++ jquery101/app.js
@@ -1,4 +1,40 @@
-var $input = $("input");
-console.log($input);
+$("#submit").click(function() {
+	var a = parseInt($("#input-1").val(), 10);
+	var b = parseInt($("#input-2").val(), 10);
+	var display = 0;
 
-$input.css('color', 'blue');
+	if ($("#operator option:selected").val() == "x") {
+		console.log(multiply(a, b), "test");
+	}
+	if ($("#operator option:selected").val() == "+") {
+		console.log(add(a, b), "test");
+	}
+	if ($("#operator option:selected").val() == "-") {
+		console.log(minus(a, b), "test");
+	}
+	if ($("#operator option:selected").val() == "/") {
+		console.log(division(a, b), "test");
+	}
+	$("#result").text
+
+});
+
+function add(a,b) {
+	return(a + +b);	
+}
+
+function division(a,b) {
+	return(a / b);
+}
+
+function minus(a,b) {
+	return(a - b);
+}
+
+function multiply(a,b) {
+	return(a * b);
+}
+/*var grr = $("select");
+console.log(grr);
+console.log($("#operator option:selected").val());*/
+/*		console.log(parseInt($("#input-2").val(), 10));*/
---
+++ jquery101/test/truc.js
@@ -0,0 +1,29 @@
+function  f(r) {
+  alert(r);
+}
+function add(x, y, callback) {
+  var result = x + y;
+  callback(result);
+}
+
+
+
+
+function iLike(pandaType,trend){
+	if(trend === "amour") {
+		alert("J'aime les pandas " + pandaType);
+	}
+	else if(pandaType === "mort" && trend == "haine")
+		alert("Satan souhaite la " + pandaType +" de tout les pandas")
+	else {
+		alert("Je n'aime pasles pandas " + pandaType);
+	}
+}
+
+$("button").click(function() {
+	var pandaType = $(this).data("type");
+	var tendance = $(this).data("tendance");
+	iLike(pandaType, tendance);
+	console.log(tendance);
+});
+console.log($("tendance"));

4b2e9348f0755ae93ce138bcc0516f55848d84d2
Mathias Ducancel <mathiasduc@gmail.com>
2016-10-07 15:05:57 +0200

    cleaned the code

--- Exercices-Js/nombreMystere/step4/index-step4.js
+++ Exercices-Js/nombreMystere/step4/index-step4.js
@@ -1,83 +1,14 @@
 $(document).ready(main);
-var lives = 3;
-var max = 0;
-var min = 0;
-var randominette = Math.floor(Math.random() * 100);
-var closeArr;
-
-function clickValider(){
-	console.log("click");
-	var inputNumb = parseInt($("#numb").val());
-
-	if ((inputNumb > randominette) && (inputNumb < closeArr[0])){
-		$("#display").html("Getting close! but Smaller!");
-		lives--;
-	}
-	else if ((inputNumb < randominette) && (inputNumb > closeArr[1])){
-		$("#display").html("Getting close! but Higher!");
-		lives--;
-	}
-	else if (inputNumb === randominette){
-		$("#display").html("THE PRICE IS RIGHT!");
-	}
-	else if (inputNumb > randominette) {
-		$("#display").html("Smaller!");
-		lives--;
-	}
-	else if (inputNumb < randominette) {
-		$("#display").html("Higher!");
-		lives--;
-	}
-}
-
-function getRandomArbitrary(min, max) {
-	return (Math.floor(Math.random() * (max - min) + min));
-}
-
-function gettingClose(randominette){
-	var percent = (randominette * 8) / 100;
-	var percentArr = [];
-	percentArr[0] = Math.floor(randominette + percent);
-	percentArr[1] = Math.floor(randominette - percent);
-	return (percentArr);
-}
-
-// Fonction principale
-function main(){
-	$("#lives").html(lives);
-	
-	$("#validate_choice").on("click", function(){
-		//l un ou l autre
-		lives = $("#lives_choice")[0].value;
-		max = $("#max_choice").val();
-		min = $("#min_choice").val();
-		max = parseInt(max , 10);
-		min = parseInt(min, 10)
-		$("#min").html(min, "min");
-		$("#max").html(max, "max");
-		$("#lives").html(lives);
-		if(max && (min !== undefined)) {
-			randominette = getRandomArbitrary(min, max);
-			closeArr = gettingClose(randominette);
-		}
-	});
-	
-	$("#validate").on("click", function(){
-		clickValider();
-		$("#lives").html(lives);
-		if (lives === 0){
-			alert("You don't have anymore lives! You lost!  Press ok to play again" );
-			lives = 3;
-			randominette = Math.floor(Math.random() * 100);
-		}
-	});
-
 	// Créer et initialiser une variable 'globale' qui 
 	// va stocker le nombre de tentatives restantes.
 
 	// Créer et initialiser une variable 'globale' qui 
 	// va stocker le nombre "aléatoire" mystère.
-
+	var lives = 3;
+	var max = 0;
+	var min = 0;
+	var randominette = 0;
+	var closeArr;
 
 	// == Fonction clickValider == 
 	// Créer la fonction 'clickValider'
@@ -109,19 +40,95 @@ function main(){
 			 		+ remettre le nombre de tentatives à sa valeur initiale
 			 		+ générer un nouveau nombre mystère
 			 		*/
+			 		function clickValider(){
+			 			var inputNumb = parseInt($("#numb").val());
 
+			 			if ((inputNumb > randominette) && (inputNumb < closeArr[0])){
+			 				$("#display").html("Getting close! but Smaller!");
+			 				lives--;
+			 			}
+			 			else if ((inputNumb < randominette) && (inputNumb > closeArr[1])){
+			 				$("#display").html("Getting close! but Higher!");
+			 				lives--;
+			 			}
+			 			else if (inputNumb === randominette){
+			 				partieGagnee();
+			 			}
+			 			else if (inputNumb > randominette) {
+			 				$("#display").html("Smaller!");
+			 				lives--;
+			 			}
+			 			else if (inputNumb < randominette) {
+			 				$("#display").html("Higher!");
+			 				lives--;
+			 			}
+			 		}
 	// /Fin == Fonction clickValider == 
 
-
-
-	// Ecrire le code qui va détecter que le bouton 'valider' est cliqué.
-	// Et qui appelle la fonction 'clickValider' défini ci-dessus
-
-
+		/*- créer une fonction 'demarrerPartie'*/
 	/*
 		Concernant la 'refactorisation' : 
-		- créer une fonction 'demarrerPartie'
 		- créer une fonction 'partieGagnee'
 		- créer une fonction 'partiePerdue'
 		*/
+	function partieGagnee(){
+			$("#display").html("THE PRICE IS RIGHT!");
+			alert("THE PRICE IS RIGHT!");
+	}
+
+	function demarrerPartie(){
+		lives = 3;
+		randominette = Math.floor(Math.random() * 100);
+		$("#lives").html(lives);
+		$("#min").html("0");
+		$("#max").html("99");
+		$("#display").html("Let's Play!");
+
+	}
+
+	function partiePerdue(){
+		alert("You don't have anymore lives! You lost!  Press ok to play again" );
+	}
+
+	function getRandomArbitrary(min, max) {
+		return (Math.floor(Math.random() * (max - min) + min));
+	}
+
+	function gettingClose(randominette){
+		var percent = (randominette * 8) / 100;
+		var percentArr = [];
+		percentArr[0] = Math.floor(randominette + percent);
+		percentArr[1] = Math.floor(randominette - percent);
+		return (percentArr);
+	}
+
+// Fonction principale
+function main(){
+	demarrerPartie()
+	$("#validate_choice").on("click", function(){
+		//l un ou l autre
+		lives = $("#lives_choice")[0].value;
+		max = $("#max_choice").val();
+		min = $("#min_choice").val();
+		max = parseInt(max , 10);
+		min = parseInt(min, 10)
+		$("#min").html(min, "min");
+		$("#max").html(max, "max");
+		$("#lives").html(lives);
+		if(max && (min !== undefined)) {
+			randominette = getRandomArbitrary(min, max);
+			closeArr = gettingClose(randominette);
+		}
+	});
+	
+	// Ecrire le code qui va détecter que le bouton 'valider' est cliqué.
+	// Et qui appelle la fonction 'clickValider' défini ci-dessus
+	$("#validate").on("click", function(){
+		clickValider();
+		$("#lives").html(lives);
+		if (lives === 0){
+			partiePerdue();
+			demarrerPartie()
+		}
+	});
 	}

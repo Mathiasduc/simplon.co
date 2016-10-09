a59491a82a21688ba3b9514e8ba64cadd03609d6
Mathias Ducancel <mathiasduc@gmail.com>
2016-10-09 10:52:09 +0200

    Add a few test and basic grid layout

---
+++ puissance4/app.js
@@ -0,0 +1,15 @@
+var player1 =  "anne" /*prompt("Player 1 name : ")*/
+var player2 =  "frank" /*prompt("Player 2 name : ")*/
+var tr_natif = document.getElementsByTagName("tr");
+console.log(tr_natif);
+var tr_jquery = $("tr");
+console.log(tr_jquery);
+/*console.log(el_tr[0].children[1].outer);*/
+
+/*el_tr[0].children[1].html("testance");*/
+
+function next_position() {
+
+}
+
+

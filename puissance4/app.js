30add4fb18b3310b9da10635082872a5390c787f
Mathias Ducancel <mathiasduc@gmail.com>
2016-10-10 06:18:53 +0200

    Add finished layout and listeners

--- puissance4/app.js
+++ puissance4/app.js
@@ -1,15 +1,21 @@
+$(document).ready(main);
 var player1 =  "anne" /*prompt("Player 1 name : ")*/
 var player2 =  "frank" /*prompt("Player 2 name : ")*/
 var tr_natif = document.getElementsByTagName("tr");
 console.log(tr_natif);
 var tr_jquery = $("tr");
 console.log(tr_jquery);
-/*console.log(el_tr[0].children[1].outer);*/
+console.log(tr_jquery[0].children[0].innerHTML);
+console.log(tr_natif[0].children[0]);
 
-/*el_tr[0].children[1].html("testance");*/
-
-function next_position() {
+function next_position(y) {
 
 }
+function main(){
 
+console.log($(".play"));
+$(".play").on("click", function(){
 
+});
+
+}
